d671ca334a51f3ef83d3fd3d22a5051efccb97fc
Sam <sameer_j@infosys.com>
2023-03-24 13:36:40 +0530

    Updated split function with basic split

--- src/model/train.py
+++ src/model/train.py
@@ -3,9 +3,9 @@
 import argparse
 import glob
 import os
-
+import numpy as np
 import pandas as pd
-
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
 
@@ -33,8 +33,11 @@ def get_csvs_df(path):
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
 
-# TO DO: add function to split data
-
+# TO DO: add function to split data - completed
+def split_data(df):
+    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    return X_train, X_test, y_train, y_test
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
